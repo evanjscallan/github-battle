51f81ed6e108cef4353465d5dc77b1b80db19020
evanjscallan <evanjscallan@gmail.com>
2022-04-15 13:28:52 -0500

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require ('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
 	entry: './app/index.js',
@@ -18,7 +19,14 @@ module.exports = {
 	plugins: [
 	new HtmlWebpackPlugin({
 		template: 'app/index.html'
-	})
+	}),
+	new CopyWebpackPlugin(
+		{
+			patterns: [
+				{ from: '_redirects' }
+			]
+		}
+		)
 	],
 	devServer: {
 		historyApiFallback: true,
